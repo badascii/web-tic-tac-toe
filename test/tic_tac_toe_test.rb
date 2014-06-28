4b40bb2235aa170aefcd863dded8315f544a565c
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-06-27 22:39:20 -0700

    got dependencies sorted

--- test/tic_tac_toe_test.rb
+++ test/tic_tac_toe_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/spec'
-
+require 'capybara'
 require_relative '../lib/tic_tac_toe'
 
 Capybara.app = TicTacToe
@@ -11,6 +11,10 @@ describe TicTacToe do
   describe 'landing page' do
     before do
       visit '/'
+    end
+
+    it 'should send a successful response' do
+      page.status_code.must_equal(200)
     end
   end
 end

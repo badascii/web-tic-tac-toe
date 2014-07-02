86523dde5d06b223b3db402a28a36f64a55822b0
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-07-01 23:51:19 -0700

    fixed grid key lookup

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,10 +6,16 @@ class TicTacToe < Sinatra::Base
            "a3" => 0, "b3" => 0, "c3" => 0}
 
   get '/' do
+    @grid ||= {"a1" => 0, "b1" => 0, "c1" => 0,
+               "a2" => 0, "b2" => 0, "c2" => 0,
+               "a3" => 0, "b3" => 0, "c3" => 0}
     erb :index
   end
 
   post '/' do
+    @grid ||= {"a1" => 0, "b1" => 0, "c1" => 0,
+               "a2" => 0, "b2" => 0, "c2" => 0,
+               "a3" => 0, "b3" => 0, "c3" => 0}
     @message = 'Movement accepted'
     @grid_location = params[:grid_location]
     @grid[@grid_location] = 'X'

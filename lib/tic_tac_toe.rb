a5c5b466e88890fccbd3bcb99cfc9d21459c3d7b
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-07-01 22:26:03 -0700

    moved grid display to instance variable

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,10 @@
 require 'sinatra/base'
 
 class TicTacToe < Sinatra::Base
+  @grid = {"a1" => 0, "b1" => 0, "c1" => 0,
+           "a2" => 0, "b2" => 0, "c2" => 0,
+           "a3" => 0, "b3" => 0, "c3" => 0}
+
   get '/' do
     erb :index
   end
@@ -8,6 +12,7 @@ class TicTacToe < Sinatra::Base
   post '/' do
     @message = 'Movement accepted'
     @grid_location = params[:grid_location]
+    @grid[@grid_location] = 'X'
     erb :index
   end
 end

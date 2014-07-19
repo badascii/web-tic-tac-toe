434570ecbdc0535c604557f05c06dc73d2d9487d
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-07-18 17:29:52 -0700

    added failing test for 2nd cpu move

--- test/tic_tac_toe_test.rb
+++ test/tic_tac_toe_test.rb
@@ -113,5 +113,15 @@ describe TicTacToe do
         page.has_content?('O').must_equal(true)
       end
     end
+
+    it 'should place the second CPU move' do
+      fill_in 'grid_location', with: 'a1'
+      click_button 'Submit'
+      fill_in 'grid_location', with: 'a2'
+      click_button 'Submit'
+      within('#b1') do
+        page.has_content?('O').must_equal(true)
+      end
+    end
   end
 end

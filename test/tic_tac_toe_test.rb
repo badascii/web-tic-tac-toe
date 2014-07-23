21b37da0a052abc81323848d8a90414c1a793f5a
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-07-22 17:14:39 -0700

    removed deprecated tests checking for player win

--- test/tic_tac_toe_test.rb
+++ test/tic_tac_toe_test.rb
@@ -76,36 +76,6 @@ describe TicTacToe do
       page.has_button?('Submit').must_equal(false)
     end
 
-    it 'should tell the player when they have a horizontal win' do
-      fill_in 'grid_location', with: 'a3'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'b3'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'c3'
-      click_button 'Submit'
-      page.has_content?('You win!').must_equal(true)
-    end
-
-    it 'should tell the player when they have a vertical win' do
-      fill_in 'grid_location', with: 'c1'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'c2'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'c3'
-      click_button 'Submit'
-      page.has_content?('You win!').must_equal(true)
-    end
-
-    it 'should tell the player when they have a diagonal win' do
-      fill_in 'grid_location', with: 'b2'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'a3'
-      click_button 'Submit'
-      fill_in 'grid_location', with: 'c1'
-      click_button 'Submit'
-      page.has_content?('You win!').must_equal(true)
-    end
-
     it 'should place the initial CPU move in B2 if open' do
       fill_in 'grid_location', with: 'a1'
       click_button 'Submit'

e1bb34e2570d9906b043fcadf35678f738fc4716
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-07-24 18:11:47 -0700

    find->replace double quote with single quotes

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,19 +3,19 @@ require 'sinatra/base'
 class TicTacToe < Sinatra::Base
   enable :sessions
 
-  GRID = {"a1" => 0, "b1" => 0, "c1" => 0,
-          "a2" => 0, "b2" => 0, "c2" => 0,
-          "a3" => 0, "b3" => 0, "c3" => 0}
+  GRID = {'a1' => 0, 'b1' => 0, 'c1' => 0,
+          'a2' => 0, 'b2' => 0, 'c2' => 0,
+          'a3' => 0, 'b3' => 0, 'c3' => 0}
 
   WIN_CONDITIONS = [
-  ["a1", "a2", "a3"], #   vertical win 0
-  ["b1", "b2", "b3"], #   vertical win 1
-  ["c1", "c2", "c3"], #   vertical win 2
-  ["a1", "b1", "c1"], # horizontal win 3
-  ["a2", "b2", "c2"], # horizontal win 4
-  ["a3", "b3", "c3"], # horizontal win 5
-  ["a1", "b2", "c3"], #   diagonal win 6
-  ["a3", "b2", "c1"]  #   diagonal win 7
+  ['a1', 'a2', 'a3'], #   vertical win 0
+  ['b1', 'b2', 'b3'], #   vertical win 1
+  ['c1', 'c2', 'c3'], #   vertical win 2
+  ['a1', 'b1', 'c1'], # horizontal win 3
+  ['a2', 'b2', 'c2'], # horizontal win 4
+  ['a3', 'b3', 'c3'], # horizontal win 5
+  ['a1', 'b2', 'c3'], #   diagonal win 6
+  ['a3', 'b2', 'c1']  #   diagonal win 7
   ]
 
   POSITION_REGEX         = /[abc][1-3]/
@@ -92,6 +92,8 @@ class TicTacToe < Sinatra::Base
       @grid[loss] = @cpu_mark
     elsif corner_defense?
       place_corner_defense
+    elsif opposite_corners?
+      @grid['a2'] = @cpu_mark
     else
       optimal_move
     end
@@ -123,10 +125,10 @@ class TicTacToe < Sinatra::Base
   end
 
   def optimal_move
-    if position_empty?("b1") && position_empty?("b3")
-      @grid["b1"] = @cpu_mark
-    elsif position_empty?("a2") && position_empty?("c2")
-      @grid["c2"] = @cpu_mark
+    if position_empty?('b1') && position_empty?('b3')
+      @grid['b1'] = @cpu_mark
+    elsif position_empty?('a2') && position_empty?('c2')
+      @grid['c2'] = @cpu_mark
     else
       @grid.each do |key, value|
         if value == 0
@@ -142,37 +144,37 @@ class TicTacToe < Sinatra::Base
   end
 
   def corner_defense?
-    side_positions = [@grid["a2"], @grid["b1"], @grid["b3"], @grid["c2"]]
+    side_positions = [@grid['a2'], @grid['b1'], @grid['b3'], @grid['c2']]
     side_positions.count(0) == 1
   end
 
   def place_corner_defense
-    if @grid["a1"] == 0
-      @grid["a1"] = @cpu_mark
-    elsif @grid["c1"] == 0
-      @grid["c1"] = @cpu_mark
-    elsif @grid["a3"] == 0
-     @grid["a3"] = @cpu_mark
-    elsif @grid["c3"] == 0
-      @grid["c3"] = @cpu_mark
+    if @grid['a1'] == 0
+      @grid['a1'] = @cpu_mark
+    elsif @grid['c1'] == 0
+      @grid['c1'] = @cpu_mark
+    elsif @grid['a3'] == 0
+     @grid['a3'] = @cpu_mark
+    elsif @grid['c3'] == 0
+      @grid['c3'] = @cpu_mark
     end
   end
 
   def side_defense?
-    corner_positions = [@grid["a1"], @grid["a3"], @grid["c1"], @grid["c3"]]
-    side_positions   = [@grid["a2"], @grid["b1"], @grid["b3"], @grid["c2"]]
-    (@grid["b2"] == @cpu_mark) && (corner_positions.uniq.count == 2) && (side_positions.uniq.count == 3)
+    corner_positions = [@grid['a1'], @grid['a3'], @grid['c1'], @grid['c3']]
+    side_positions   = [@grid['a2'], @grid['b1'], @grid['b3'], @grid['c2']]
+    (@grid['b2'] == @cpu_mark) && (corner_positions.uniq.count == 2) && (side_positions.uniq.count == 3)
   end
 
   def place_side_defense
-    if @grid["a2"] == 0
-      @grid["a2"] = @cpu_mark
-    elsif @grid["b1"] == 0
-      @grid["b1"] = @cpu_mark
-    elsif @grid["b3"] == 0
-      @grid["b3"] = @cpu_mark
-    elsif @grid["c2"] == 0
-      @grid["c2"] = @cpu_mark
+    if @grid['a2'] == 0
+      @grid['a2'] = @cpu_mark
+    elsif @grid['b1'] == 0
+      @grid['b1'] = @cpu_mark
+    elsif @grid['b3'] == 0
+      @grid['b3'] = @cpu_mark
+    elsif @grid['c2'] == 0
+      @grid['c2'] = @cpu_mark
     end
   end
 end

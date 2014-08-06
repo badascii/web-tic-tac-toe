d14b7bd2233fa36a811b10c1bcb53e7f26c1506a
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-08-06 15:21:04 -0700

    fixed message displaying location of player move

--- lib/app.rb
+++ lib/app.rb
@@ -21,9 +21,8 @@ class TicTacToe < Sinatra::Base
   post '/game/move' do
     game = Game.new(session)
 
-    player_move = params[:grid_position]
-
-    game.round(player_move)
+    @player_move = params[:grid_position]
+    game.round(@player_move)
     session['grid'] = game.grid
     @mode    = game.mode
     @grid    = game.grid

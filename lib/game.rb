46b4c03026f374d84764d7880c424dd2859264c9
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-08-27 14:13:30 -0700

    implemented method to switch game size based on player input

--- lib/game.rb
+++ lib/game.rb
@@ -6,26 +6,26 @@ class Game
   attr_accessor :grid, :player_1, :player_2, :cpu, :mode, :size, :turn, :message, :result
 
   def initialize(opts)
-    @size = opts[:size]
-    @mode = opts[:mode]
-    @turn = opts[:turn]
+    @size     = opts[:size]
+    @mode     = opts[:mode]
+    @turn     = opts[:turn]
     @player_1 = 'X'
     @player_2 = 'O'
-    @cpu = 'O'
-    @result = nil
-    @grid = get_grid(@size)
-    @message = opts[:message] || 'Welcome to the Fields of Strife'
+    @cpu      = 'O'
+    @result   = nil
+    @grid     = get_grid(@size)
+    @message  = opts[:message] || 'Welcome to the Fields of Strife'
   end
 
   def round(position)
-    game = game_class.new(session_hash)
+    game     = game_class.new(session_hash)
     game.round(position)
-    @grid = game.grid
-    @mode = game.mode
-    @size = game.size
-    @turn = game.turn
+    @grid    = game.grid
+    @mode    = game.mode
+    @size    = game.size
+    @turn    = game.turn
     @message = game.message
-    @result = game.result
+    @result  = game.result
   end
 
   def session_hash
@@ -39,6 +39,14 @@ class Game
   end
 
   private
+
+  def game_class
+    if @size == '4x4'
+      Game4x4
+    else
+      Game3x3
+    end
+  end
 
   def get_grid(size)
     if size == '4x4'
